cf14ac779b0bbfa3c832fe2b3729a33442491a07
Davis Stöwer <davis.stoewer@gmail.com>
2025-11-26 16:34:26 +0100

    Adapted c_make_hawc2 to create turbulent simulations for the controller evaluation

--- hawc_files/c_make_hawc2.py
+++ hawc_files/c_make_hawc2.py
@@ -1,77 +1,73 @@
-"""Use control tuning files to make step HAWC2 files.
-
-This file should create the following htc files:
- * _fX_dY_C[T/P].htc (X files)
-
-Note that you need to create some functions in myteampack that should
-be called in this script.
-"""
-from collections import namedtuple
 from pathlib import Path
+import json
+import re
+import random
 
-import numpy as np
-
+from lacbox.htc import _clean_directory
 from lacbox.io import load_ctrl_txt
+
 from myteampack import MyHTC
 
-# define folders
+# %% User inputs
+DESIGN_NAME = "IEC_What_You_Did_There"
+wsp = 24
+nseeds = 10
+minspeed = 0  # Minimum rotor speed [rad/s]
+
+# %% Path preparations and file loading
 ROOT = Path(__file__).parent
-DESIGN_NAME = "IEC_Ya_Later"
-MASTER_FILE = ROOT / '_master' / f'{DESIGN_NAME}.htc'  # your master htc file
-RES_DIR = ROOT / Path('./res_hawc2s_ctrl/')  # location of _ctrl_tunint.txt files
-HAWC2_HTC_DIR = ROOT / Path('./htc_hawc2/htc_step/')  # where to save the step-wind files
+MASTER_FILE = ROOT / '_master' / f'{DESIGN_NAME}.htc'
+CTRL_DIR = ROOT / 'res' / 'hawc2s' / 'ctrltune'
+OPT_PATH_FLEX = ROOT / 'data' / f'{DESIGN_NAME}_flex.opt'
+RES_DIR = ROOT / 'res' / 'hawc2' / 'ctrltune_eval'  # HTC save folder
+HTC_DIR = Path('./htc/hawc2/ctrleval/')  # where to save the step-wind files
 
-# Rated speed
-rpm_HSS = 403.1  # HSS rated rotational speed [rpm]
-n_gear = 50  # Gear ratio
-omega_rtd_LSS = rpm_HSS/n_gear*2*np.pi/60
+TI_REF = .14  # Turbulence intensities for IEC  B
+TI = TI_REF*(0.75*wsp+5.6)/wsp
+TIME_START = 100
+TIME_STOP = 700
+START_SEED = 42  # initialize the random-number generator for reproducability
 
-# step-wind settings
-CUTIN, CUTOUT = 4, 25
-DT, TSTART = 100, 100
+# Load design parameters
+with open(ROOT / "data" / f"{DESIGN_NAME}_params.json", 'r') as file:
+    des_params = json.load(file)
 
-# get a list of all _ctrl_tuning.txt files...
-ctrl_tuning_file = namedtuple("ctrl_tuning_file", ["omega", "zeta", "params"])
-# omega = [.05, .01, .1]*2
-# zeta = [.7]*6
-# CTCP = ["CP"]*3 + ["CT"]*3
+# Read all control tuning files
+ctrl_tune_files = []
+for file in CTRL_DIR.iterdir():
+    if file.name.endswith("ctrl_tuning.txt"):
+        ctrl_tune_files.append(file)
 
-omega = [.05]
-zeta = [.7]
-CTCP = ["CP"]
+if len(ctrl_tune_files) == 0:
+    raise OSError("No control tuning files found.")
 
-rpm_HSS = 403.1   # HSS rated rotational speed [rpm]
-n_gear = 50
-omega_rtd_LSS = rpm_HSS/n_gear*2*np.pi/60
+# clean the top-level htc directory if requested
+_clean_directory(HTC_DIR, True)
 
-ctrl_basepath = DESIGN_NAME + \
-    "_hawc2s_flex_ctrltune_{CPCT}_f{omega:g}_Z{zeta:g}_ctrl_tuning.txt"
-ctrl_tuning_files = [
-    ctrl_tuning_file(o, z,
-                     load_ctrl_txt(Path(RES_DIR, ctrl_basepath.format(CPCT=cpct,
-                                                                      omega=o,
-                                                                      zeta=z)))
-                     )
-    for cpct, o, z in zip(CTCP, omega, zeta)]
+# Create turbulent htc files
+for ctrl_tune_file in ctrl_tune_files:
+    ctrl_params = load_ctrl_txt(ctrl_tune_file)
+    freq, damp = re.findall(r"f(0.\d+)_Z(0.\d+)", ctrl_tune_file.name)[0]
 
-# Create HTC files
-wsp_steps = np.arange(CUTIN+1, CUTOUT+1, 1)
-step_times = np.arange(1, len(wsp_steps)+1, 1)*DT + TSTART
-t_end = step_times[-1] + DT
-for i, ctrl_tuning_file in enumerate(ctrl_tuning_files):
-    # load the master htc file
-    htc = MyHTC(MASTER_FILE)
+    for idx_seed in range(nseeds):
+        sim_seed = random.randrange(int(2**16))
+        htc = MyHTC(MASTER_FILE)
 
-    # update controller block in htc file
-    htc._update_ctrl_params(ctrl_tuning_file.params,
-                            rated_rot_speed=omega_rtd_LSS)
+        # update controller block in htc file
+        htc._update_ctrl_params(
+            ctrl_params, min_rot_speed=minspeed,
+            rated_rot_speed=des_params["omega_rot_rtd_rads"])
 
-    # generate a step-wind HAWC2 file
-    # htc.make_step(save_dir=HAWC2_HTC_DIR,
-    #               wsp=4, wsp_steps=wsp_steps, step_times=step_times,
-    #               last_step_len=DT, start_record_time=TSTART,
-    #               append=f"_hawc2s_flex_ctrleval_C{i+1}")
-    htc.make_step(save_dir=HAWC2_HTC_DIR,
-                  wsp=4, wsp_steps=wsp_steps, step_times=step_times,
-                  last_step_len=DT, start_record_time=TSTART,
-                  append=f"_hawc2s_flex_ctrleval_C{1}")
+        # Update rotor diameter in controller dll
+        r_hub = htc.new_htc_structure.main_body__4.c2_def.sec__2.values[-2]
+        l_bld = htc.new_htc_structure.main_body__7.c2_def.sec__27.values[-2]
+        r_rotor = r_hub + l_bld
+        htc.dll.type2_dll__1.init.constant__47 = [47, 2*r_rotor]
+
+        htc.make_turb(wsp=wsp, ti=TI, seed=sim_seed,
+                      append=f"_f{freq}_Z{damp}_{sim_seed:d}",
+                      save_dir=HTC_DIR, subfolder="",
+                      opt_path=OPT_PATH_FLEX, resdir=RES_DIR,
+                      dy=190, dz=190,
+                      rigid=False, withdrag=True, tilt=None,
+                      time_start=TIME_START, time_stop=TIME_STOP)
